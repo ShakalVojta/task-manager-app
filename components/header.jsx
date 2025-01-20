5341a2f60ab1db851f1352fda14c539e27050ec7
Vojtěch Týnavský <vojtech.tynavsky@seznam.cz>
2025-01-20 23:09:22 +0100

    creating landing page, models with prisma and storing user data in DB

--- components/header.jsx
+++ components/header.jsx
@@ -1,16 +1,47 @@
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs"
+import { SignedIn, SignedOut, SignInButton } from "@clerk/nextjs"
+import Image from "next/image"
+import Link from "next/link"
+import Logo2 from "../public/logo2.png"
+import { Button } from "./ui/button"
+import { PenBox } from "lucide-react"
+import UserMenu from "./user-menu"
+import { checkUser } from "@/lib/checkUser"
 
-const Header = () => {
+const Header = async () => {
+await checkUser()
+
+
   return (
-    <div>
+    <header className="container mx-auto">
+      <nav className="py-6 px-4 flex justify-between items-center">
+        <Link href='/'>
+        <Image src={Logo2} alt="Zscrum Logo"
+        width={200}
+        height={56}
+        className="h-10 w-auto objec-contain"
+        />
+        </Link>
+
+    <div className="flex items-center gap-4">
+    <Link href='/project/create'>
+    <Button variant="destructive" className="flex items-center gap-2">
+      <PenBox size={18} />
+      <span>Create Project</span>
+    </Button>
+    </Link>
+
      <SignedOut>
-        <SignInButton />
+        <SignInButton forceRedirectUrl="/onboarding">
+        <Button variant="outline">Login</Button>
+        </SignInButton>
      </SignedOut>
 
      <SignedIn>
-        <UserButton />
+        <UserMenu />
      </SignedIn>
-    </div>
+     </div>
+     </nav>
+    </header>
   )
 }
 
