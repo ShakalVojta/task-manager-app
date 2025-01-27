5d4b5bd6ad77bc750e729c9ece9bc0728268587f
Vojtěch Týnavský <vojtech.tynavsky@seznam.cz>
2025-01-27 17:53:15 +0100

    fix small errors

--- app/layout.js
+++ app/layout.js
@@ -29,15 +29,32 @@ export default function RootLayout({ children }) {
           card: "bg-gray-800",
           headerTitle: "text-blue-400",
           headerSubtitle: "text-gray-400",
-        }
+        },
       }}
     >
-      <html lang="en">
-        <body className={`${inter.className} dotted-background`}>
-          <ThemeProvider attribute="class" defaultTheme="dark">
+      <html lang="en" suppressHydrationWarning>
+        <body
+          className={`${inter.className} dotted-background`}
+          suppressHydrationWarning
+        >
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem={true}
+            disableTransitionOnChange
+          >
             <Header />
             <main className="min-h-screen">{children}</main>
-            <Toaster richColors=""/>
+            <Toaster
+              richColors
+              theme="dark"
+              toastOptions={{
+                style: {
+                  background: "#22c55e",
+                  color: "white",
+                },
+              }}
+            />
             <footer className="bg-gray-900 py-12">
               <div className="container mx-auto px-4 text-center text-gray-200">
                 <p>This is footer from ShakalTyny ❤️ </p>
